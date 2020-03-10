956e36d36dabde8d6f130cbb934f46fca1a612b9
Natanael Saymon <nsaymonsouza@gmail.com>
2020-03-10 14:11:26 -0300

    configurando arquivos .njk

---
+++ Fase-02/03-Iniciando-no-back-end/public/scripts.js
@@ -0,0 +1,18 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+
+const cards = document.querySelectorAll('.card')
+
+for(let card of cards){
+    card.addEventListener("click", function(){
+        const videoId = card.getAttribute('id') /* Pegar o atributo ID*/
+        modalOverlay.classList.add('active')
+        modalOverlay.querySelector('iframe').src = `https://youtube.com/embed/${videoId}`
+    })
+}
+
+/*isnerindo a função de fechar o modal*/
+document.querySelector('.close-modal').addEventListener("click", function(){
+    modalOverlay.classList.remove('active')
+    modalOverlay.querySelector('iframe').src = "" /* remove o video */
+})
+
--- Fase-02/03-Iniciando-no-back-end/server.js
+++ Fase-02/03-Iniciando-no-back-end/server.js
@@ -1,5 +1,23 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views" ,{
+    express:server
+})
+
+server.get("/", function(req, res){
+    return res.render('about')
+})
+
+server.get("/portfolio", function(req, res){
+    return res.render('portfolio')
+})
 
 server.listen(5000, function(){
     console.log("server is runnign")
